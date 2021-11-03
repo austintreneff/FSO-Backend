d98594b022e4e8749c9e5b99c539ffefcf67f31c
austintreneff <austintreneff@gmail.com>
2021-11-03 18:07:40 -0400

    setup backend for deployment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var morgan = require('morgan')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('request', (req, res) => {
   return JSON.stringify(req.body)
@@ -110,7 +113,7 @@ const userExists = (name) => {
 }
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
